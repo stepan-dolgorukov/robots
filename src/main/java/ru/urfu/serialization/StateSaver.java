f727c6b84076c644aeaf8c33fadf5eff0212a2f0
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-03-19 19:38:33 +0500

    Метод для совместного сохранения объектов

--- src/main/java/ru/urfu/serialization/StateSaver.java
+++ src/main/java/ru/urfu/serialization/StateSaver.java
@@ -13,4 +13,10 @@ abstract  public class StateSaver {
      * @param obj объект, который хотим сохранить
      */
     abstract public void save(Saveable obj);
+
+    /**
+     * Сохранение состояний нескольких объектов.
+     * @param objs объекты, которые хотим сохранить
+     */
+    abstract public void save(List<Saveable> objs);
 }
