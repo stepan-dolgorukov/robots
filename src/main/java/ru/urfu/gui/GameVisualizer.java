f264f3af5a29a53605107d6343812329a92cda94
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-04-04 15:42:45 +0500

    Уменьшение количества строк импорта

--- src/main/java/ru/urfu/gui/GameVisualizer.java
+++ src/main/java/ru/urfu/gui/GameVisualizer.java
@@ -1,10 +1,6 @@
 package ru.urfu.gui;
 
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
