3df81bf33ee402c713c3d685b12922a188aad546
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-02 20:33:11 +0500

    Нормальное оформление Контроллера и Модели

--- src/main/java/ru/urfu/controller/RobotController.java
+++ src/main/java/ru/urfu/controller/RobotController.java
@@ -4,14 +4,15 @@ import ru.urfu.model.RobotModel;
 
 
 import java.awt.geom.Point2D;
+import java.nio.file.attribute.PosixFileAttributes;
 import java.util.Observer;
 import ru.urfu.model.RobotInfo;
 
 public class RobotController {
     private final RobotModel model_;
 
-    public RobotController(final RobotModel model) {
-        model_ = model;
+    public RobotController(final RobotInfo info) {
+        model_ = new RobotModel(info);
     }
 
     public void addSubscriber(final Observer modelSubscriber) {
@@ -27,10 +28,163 @@ public class RobotController {
             return;
         }
 
-        model_.update(targetPosition);
+        update(targetPosition);
     }
 
     public void requestModelInfo() {
-        model_.info();
+        model_.notifySubscribers();
+    }
+
+    private void update(Point2D.Double targetNewPosition) {
+        final Point2D.Double position = model_.getPosition();
+        final Point2D.Double targetPosition = model_.getTargetPosition();
+        final double direction = model_.getDirection();
+
+        final double distance = distance(position, targetNewPosition);
+
+        if (distance < 0.5) {
+            return;
+        }
+
+        final double velocity = model_.getMaxVelocity();
+        final double angleToTarget = angleTo(position.getX(),
+                position.getY(),
+                targetPosition.getX(),
+                targetPosition.getY());
+
+        double angularVelocity = 0.0;
+
+        if (angleToTarget > direction)
+        {
+            angularVelocity = model_.getMaxAngularVelocity();
+        }
+
+        if (angleToTarget < direction)
+        {
+            angularVelocity = -model_.getMaxAngularVelocity();
+        }
+
+        model_.setTargetPosition(targetNewPosition);
+        moveRobot(velocity, angularVelocity, 10);
+    }
+
+    /**
+     * Высчитать направление & координаты робота. Результаты
+     * сохраняются в информацию о модели. Всем подписчикам приходит
+     * обновление.
+
+     * @param velocity скорость
+     * @param angularVelocity угловая скорость
+     * @param duration сколько планируется двигаться в миллисекундах
+     */
+    private void moveRobot(final double velocity, final double angularVelocity,
+                           final double duration) {
+
+        final Point2D.Double position = model_.getPosition();
+        final double maxVelocity = model_.getMaxVelocity();
+        final double maxAngularVelocity = model_.getMaxAngularVelocity();
+        final double direction = model_.getDirection();
+
+//        System.err.println(direction);
+
+        final double newVelocity = applyLimits(velocity, 0, maxVelocity);
+        final double newAngularVelocity =  applyLimits(angularVelocity,
+                -maxAngularVelocity, maxAngularVelocity);
+
+        double newX = position.getX() + newVelocity / newAngularVelocity *
+                (Math.sin(direction + newAngularVelocity * duration) -
+                        Math.sin(direction));
+
+        System.err.println(newX);
+
+        if (!Double.isFinite(newX)) {
+            newX = position.getX() +
+                    newVelocity * duration * Math.cos(direction);
+        }
+
+        double newY = position.getY() - newVelocity / newAngularVelocity *
+                (Math.cos(direction + newAngularVelocity * duration) -
+                        Math.cos(direction));
+
+        if (!Double.isFinite(newY)) {
+            newY = position.getY() +
+                    newVelocity * duration * Math.sin(direction);
+        }
+
+        model_.setPosition(new Point2D.Double(newX, newY));
+
+        final double newDirection =
+                asNormalizedRadians(direction + newAngularVelocity * duration);
+
+        model_.setDirection(newDirection);
+        model_.notifySubscribers();
+    }
+
+    /**
+     * Поместить значение угла в радианах в полуинтервал [0; 2π) — нормализация
+     * @param angle значение угла в радианах
+     * @return нормализованное значение
+     **/
+    private double asNormalizedRadians(double angle) {
+        final double doublePi = 2 * Math.PI;
+
+        while (angle < 0) {
+            angle += doublePi;
+        }
+
+        while (angle >= doublePi) {
+            angle -= doublePi;
+        }
+
+        return angle;
+    }
+
+    /**
+     * Расстояние между двумя точками.
+     * @param first первая точка
+     * @param second вторая точка
+     * @return расстояние
+     */
+    private double distance(final Point2D.Double first,
+                            final Point2D.Double second)
+    {
+        final double firstAbscissa = first.getX();
+        final double firstOrdinate = first.getY();
+
+        final double secondAbscissa = second.getX();
+        final double secondOrdinate = second.getY();
+
+        final double abscissaDiff = firstAbscissa - secondAbscissa;
+        final double ordinateDiff = firstOrdinate - secondOrdinate;
+
+        return Math.hypot(abscissaDiff, ordinateDiff);
+    }
+
+    /**
+     * Угол между точками: под каким углом проходит прямая, соединяющая точки
+     **/
+    private double angleTo(final double fromX, final double fromY,
+                           final double toX, final double toY)
+    {
+        final double diffX = toX - fromX;
+        final double diffY = toY - fromY;
+
+        return asNormalizedRadians(Math.atan2(diffY, diffX));
+    }
+
+    /**
+     * Применить левые и правые границы на значение.
+     * @param value значение, на которое накладываем границы
+     * @param min левая граница
+     * @param max правая граница
+     */
+    private static double applyLimits(final double value, final double min,
+                                      final double max)
+    {
+        if (value < min) {
+            return min;
+        }
+
+        return Math.min(value, max);
     }
 }
--- src/main/java/ru/urfu/model/RobotModel.java
+++ src/main/java/ru/urfu/model/RobotModel.java
@@ -3,261 +3,125 @@ package ru.urfu.model;
 import java.awt.geom.Point2D;
 import java.util.Observable;
 
-/**
- * Модель робота.
- */
 public class RobotModel extends Observable {
+
     /**
-     * Информация о роботе:
-     *  где находится,
-     *  куда едет,
-     *  максимальная скорость,
-     *  максимальная угловая скорость
+     * Где находится робот.
      */
-    private class RobotInfoEditable extends RobotInfo {
+    private Point2D.Double position_;
 
-        public RobotInfoEditable(Point2D.Double position,
-                                 Point2D.Double targetPosition, double velocity, double maxAngularVelocity) {
-            super(position, targetPosition, velocity, maxAngularVelocity);
-        }
+    /**
+     * Где находится цель робота (куда он едет).
+     */
+    private Point2D.Double targetPosition_;
 
-        /**
-         * Установить позицию роботу.
-         * @param position новая позиция
-         */
-        public void setPosition(final Point2D.Double position) {
-            super.position_ = position;
-        }
+    /**
+     * Максимальная скорость.
+     */
+    private double maxVelocity_ = 0.1;
 
-        /**
-         * Обновить позицию цели для робота.
-         * @param position новая позиция цели.
-         */
-        public void setTargetPosition(final Point2D.Double position) {
-            targetPosition_ = position;
-        }
+    /**
+     * Максимальная угловая скорость.
+     */
+    private double maxAngularVelocity_ = 0.001;
 
-        /**
-         * Обновить направление, в котором двигается робот.
-         * Направление задаётся углом. Угол в радианах.
-         * @param direction угол
-         */
-        public void setDirection(double direction) {
-            direction_ = direction;
-        }
+    /**
+     * Направление.
+     */
+    private double direction_ = 0.0;
 
-        /**
-         * Обновить максимальную скорость движения робота.
-         * @param velocity новая скорость
-         */
-        public void setMaxVelocity(double velocity) {
-            maxVelocity_ = velocity;
-        }
 
-        /**
-         * Установить максимальную угловую скорость движения робота.
-         * @param velocity новая скорость
-         */
-        public void setMaxAngularVelocity(double velocity) {
-            maxAngularVelocity_ = velocity;
-        }
+    public RobotModel(Point2D.Double position, Point2D.Double targetPosition) {
+
+        position_ = position;
+        targetPosition_ = targetPosition;
+    }
+
+    public RobotModel(RobotModel other) {
+        position_ = other.getPosition();
+        targetPosition_ = other.getTargetPosition();
+        maxVelocity_ = other.getMaxVelocity();
+        maxAngularVelocity_ = other.getMaxAngularVelocity();
+        direction_ = other.getDirection();
+    }
+
+    public RobotModel(RobotInfo info) {
+        position_ = info.getPosition();
+        targetPosition_ = info.getTargetPosition();
+        maxVelocity_ = info.getMaxVelocity();
+        maxAngularVelocity_ = info.getMaxAngularVelocity();
+        direction_ = info.getDirection();
     }
 
     /**
-     * Объект, характеризующий состояние робота.
+     * Узнать позицию робота.
      */
-    final RobotInfoEditable robotInfo_;
-
-    public RobotModel() {
-
-        {
-            final Point2D.Double position = new Point2D.Double(100.0,
-                    100.0);
-            final Point2D.Double targetPosition = new Point2D.Double(100.0, 100.0);
-            final double maxVelocity = 0.1;
-            final double maxAngularVelocity = 0.001;
-
-            robotInfo_ = new RobotInfoEditable(position, targetPosition,
-                    maxVelocity,
-                    maxAngularVelocity);
-        }
+    public Point2D.Double getPosition() {
+        return position_;
     }
 
     /**
-     * Расстояние между двумя точками.
-     * @param first первая точка
-     * @param second вторая точка
-     * @return расстояние
+     * Получить позицию цели.
+     * @return точка, в которой находится цель
      */
-    private static double distance(final Point2D.Double first,
-                                   final Point2D.Double second)
-    {
-        final double firstAbscissa = first.getX();
-        final double firstOrdinate = first.getY();
-
-        final double secondAbscissa = second.getX();
-        final double secondOrdinate = second.getY();
-
-        final double abscissaDiff = firstAbscissa - secondAbscissa;
-        final double ordinateDiff = firstOrdinate - secondOrdinate;
-
-        return Math.hypot(abscissaDiff, ordinateDiff);
+    public Point2D.Double getTargetPosition() {
+        return targetPosition_;
     }
 
     /**
-     * Угол между точками: под каким углом проходит прямая, соединяющая точки
-     **/
-    private static double angleTo(final double fromX, final double fromY,
-                                  final double toX, final double toY)
-    {
-        final double diffX = toX - fromX;
-        final double diffY = toY - fromY;
-
-        return asNormalizedRadians(Math.atan2(diffY, diffX));
+     * Получить направление.
+     * @return угол, задающий направление
+     */
+    public double getDirection() {
+        return direction_;
     }
 
     /**
-     * Отправить подписчикам нынешнее состояние модели.
+     * Узнать максимальную скорость движения робота.
+     * @return скорость
      */
-    public void info() {
+    public double getMaxVelocity() {
+        return maxVelocity_;
+    }
+
+    /**
+     * Получить максимальную угловую скорость.
+     * @return скорость
+     */
+    public double getMaxAngularVelocity() {
+        return maxAngularVelocity_;
+    }
+
+    /**
+     * Установить позицию роботу.
+     * @param newPosition новая позиция
+     */
+    public void setPosition(Point2D.Double newPosition) {
+        position_ = newPosition;
+    }
+
+    /**
+     * Обновить направление, в котором двигается робот.
+     * Направление задаётся углом. Угол в радианах.
+     * @param newDirection угол
+     */
+    public void setDirection(double newDirection) {
+        direction_ = newDirection;
+    }
+
+    /**
+     * Разослать подписчикам актуальную информацию.
+     */
+    public void notifySubscribers() {
         setChanged();
-        notifyObservers(robotInfo_);
+        notifyObservers(new RobotInfo(this));
     }
 
     /**
-     * Обновить модель: установить новые координаты цели.
-     * @param targetNewPosition пара координат X, Y
+     * Обновить позицию цели для робота.
+     * @param targetNewPosition новая позиция цели
      */
-    public void update(Point2D.Double targetNewPosition) {
-        final Point2D.Double position = robotInfo_.getPosition();
-        final Point2D.Double targetPosition = robotInfo_.getTargetPosition();
-        final double direction = robotInfo_.getDirection();
-
-        final double distance = distance(position, targetNewPosition);
-
-        if (distance < 0.5) {
-            return;
-        }
-
-        final double velocity = robotInfo_.getMaxVelocity();
-        final double angleToTarget = angleTo(position.getX(),
-                position.getY(),
-                targetPosition.getX(),
-                targetPosition.getY());
-
-        double angularVelocity = 0.0;
-
-        if (angleToTarget > direction)
-        {
-            angularVelocity = robotInfo_.getMaxAngularVelocity();
-        }
-
-        if (angleToTarget < direction)
-        {
-            angularVelocity = -robotInfo_.getMaxAngularVelocity();
-        }
-
-        robotInfo_.setTargetPosition(targetNewPosition);
-        moveRobot(velocity, angularVelocity, 10);
-    }
-
-    /**
-     * Применить левые и правые границы на значение.
-     * @param value значение, на которое накладываем границы
-     * @param min левая граница
-     * @param max правая граница
-     */
-    private static double applyLimits(final double value, final double min,
-                                      final double max)
-    {
-        if (value < min) {
-            return min;
-        }
-
-        return Math.min(value, max);
-    }
-
-    /**
-     * Высчитать направление & координаты робота. Результаты
-     * сохраняются в информацию о модели. Всем подписчикам приходит
-     * обновление.
-
-     * @param velocity скорость
-     * @param angularVelocity угловая скорость
-     * @param duration сколько планируется двигаться в миллисекундах
-     */
-    private void moveRobot(final double velocity, final double angularVelocity,
-                           final double duration) {
-
-        final Point2D.Double position = robotInfo_.getPosition();
-        final double maxVelocity = robotInfo_.getMaxVelocity();
-        final double maxAngularVelocity = robotInfo_.getMaxAngularVelocity();
-        final double direction = robotInfo_.getDirection();
-
-//        System.err.println(direction);
-
-        final double newVelocity = applyLimits(velocity, 0, maxVelocity);
-        final double newAngularVelocity =  applyLimits(angularVelocity,
-                -maxAngularVelocity, maxAngularVelocity);
-
-        double newX = position.getX() + newVelocity / newAngularVelocity *
-                (Math.sin(direction + newAngularVelocity * duration) -
-                        Math.sin(direction));
-
-        System.err.println(newX);
-
-        if (!Double.isFinite(newX)) {
-            newX = position.getX() +
-                    newVelocity * duration * Math.cos(direction);
-        }
-
-        double newY = position.getY() - newVelocity / newAngularVelocity *
-                (Math.cos(direction + newAngularVelocity * duration) -
-                        Math.cos(direction));
-
-        if (!Double.isFinite(newY)) {
-            newY = position.getY() +
-                    newVelocity * duration * Math.sin(direction);
-        }
-
-        robotInfo_.setPosition(new Point2D.Double(newX, newY));
-
-        final double newDirection =
-                asNormalizedRadians(direction + newAngularVelocity * duration);
-
-        robotInfo_.setDirection(newDirection);
-
-        setChanged();
-        notifyObservers(robotInfo_);
-    }
-
-    /**
-     * Поместить значение угла в радианах в полуинтервал [0; 2π) — нормализация
-     * @param angle значение угла в радианах
-     * @return нормализованное значение
-     **/
-    private static double asNormalizedRadians(double angle) {
-        final double doublePi = 2 * Math.PI;
-
-        while (angle < 0) {
-            angle += doublePi;
-        }
-
-        while (angle >= doublePi) {
-            angle -= doublePi;
-        }
-
-        return angle;
-    }
-
-    /**
-     * Человеческое округление.
-     * Если дробная часть ≥0.5, то вверх, иначе вниз.
-     * @param value округляемое значение
-     * @return округлённое значение
-     */
-    private static int round(final double value)
-    {
-        return (int)(value + 0.5);
+    public void setTargetPosition(Point2D.Double targetNewPosition) {
+        targetPosition_ = targetNewPosition;
     }
 }
